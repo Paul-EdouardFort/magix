b64cf0ba9944836a894dc90f7ec9a91994c33558
Paul-Édouard Fortin <fortinpauledouard@gmail.com>
2025-11-14 10:27:48 -0500

    functional game!!

--- magix-frontend/src/components/card.jsx
+++ magix-frontend/src/components/card.jsx
@@ -2,17 +2,18 @@ import cardData from "../data/cardData"
 import inventory from "../assets/img/Inventory_tab.png"
 export default function Card({id=0,cost=0,hp=0,atk=0,mechanics=[],uid=0,baseHP,onClick}) {
     let card_data = cardData(id);
-    return <div onClick={() => onClick(uid)} className="w-[10vw] h-[25vh] bg-[url(../assets/img/Inventory_tab.png)] bg-cover flex flex-none flex-col text-center outline-offset-[-2px] p-2"  >
+    return <div onClick={() => onClick(uid)} className="w-[10vw] h-[25vh] bg-[url(../assets/img/Inventory_tab.png)] bg-cover bg-no-repeat text-yellow-300 flex flex-none flex-col text-center outline-offset-[-2px] p-2"  >
         <div className="h-[15%] flex flex-none flex-row justify-between">
-            <p>{atk}</p>
+            <p>atk : {atk}</p>
             <p className="text-xl">{card_data["name"]}</p>
-            <p>{hp}</p>
+            <p>hp : {hp}</p>
         </div>
         <div className="h-[50%] flex-none pl-[10%] pr-[10%] border-2 bg-stone-500">
             <img className="object-contain w-full h-full" src={card_data["image"]}></img> 
         </div>
         <div className="h-[35%] flex-none text-left overflow-hidden">
-            <p>{cost}</p>
+            <p>coût : {cost}</p>
+            <p>id : {uid}</p>
             <p className="text-sm">{mechanics}</p>
         </div>
     </div>
